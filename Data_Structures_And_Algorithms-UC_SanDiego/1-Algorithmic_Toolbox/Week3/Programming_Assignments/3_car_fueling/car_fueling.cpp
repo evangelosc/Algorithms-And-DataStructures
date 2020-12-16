bb29e8f782e6900558cd0cd797ae8ad536e776f6
Evangelos Charalampidis <evan.charalampidis@gmail.com>
2020-12-16 22:22:20 +0000

    Week 3 Assignment 3

--- Data_Structures_And_Algorithms-UC_SanDiego/1-Algorithmic_Toolbox/Week3/Programming_Assignments/3_car_fueling/car_fueling.cpp
+++ Data_Structures_And_Algorithms-UC_SanDiego/1-Algorithmic_Toolbox/Week3/Programming_Assignments/3_car_fueling/car_fueling.cpp
@@ -7,8 +7,24 @@ using std::vector;
 using std::max;
 
 int compute_min_refills(int dist, int tank, vector<int> & stops) {
-    // write your code here
-    return -1;
+    auto it = stops.insert(stops.begin(),0);
+    stops.push_back(dist);
+    int numberRefills = 0;
+    int currentRefill = 0;
+    int lastRefill;
+    while (currentRefill<=stops.size()-2){
+        lastRefill = currentRefill;
+        while((currentRefill<=stops.size()-2) && ((stops[currentRefill+1]-stops[currentRefill]) <= tank )){
+            currentRefill += 1;
+        }
+        if (currentRefill==lastRefill){
+            return -1;
+        }
+        if (currentRefill<=stops.size()-2){
+            numberRefills += 1;
+        }
+    }
+    return numberRefills;
 }
 
 
@@ -23,7 +39,6 @@ int main() {
     vector<int> stops(n);
     for (size_t i = 0; i < n; ++i)
         cin >> stops.at(i);
-
     cout << compute_min_refills(d, m, stops) << "\n";
 
     return 0;
--- Data_Structures_And_Algorithms-UC_SanDiego/1-Algorithmic_Toolbox/Week3/Programming_Assignments/3_car_fueling/car_fueling.py
+++ Data_Structures_And_Algorithms-UC_SanDiego/1-Algorithmic_Toolbox/Week3/Programming_Assignments/3_car_fueling/car_fueling.py
@@ -3,8 +3,18 @@ import sys
 
 
 def compute_min_refills(distance, tank, stops):
-    # write your code here
-    return -1
+    current = 0
+    count = 0
+    stops = [0] + stops + [distance]
+    while current <= len(stops)-2:
+        last = current
+        while current <= len(stops)-2 and stops[current + 1] - stops[last] <= tank:
+            current = current + 1
+        if current == last:
+            return -1
+        if current <= len(stops)-2:
+            count = count + 1
+    return count
 
 if __name__ == '__main__':
     d, m, _, *stops = map(int, sys.stdin.read().split())
