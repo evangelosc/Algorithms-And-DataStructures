fe9293df33524a42e792670555fa937eb432458e
Evangelos Charalampidis <evan.charalampidis@gmail.com>
2020-12-19 20:26:32 +0000

    Week 4 Assignment 3

--- Data_Structures_And_Algorithms-UC_SanDiego/1-Algorithmic_Toolbox/Week4/Programming_Assignments/3_improving_quicksort/sorting.cpp
+++ Data_Structures_And_Algorithms-UC_SanDiego/1-Algorithmic_Toolbox/Week4/Programming_Assignments/3_improving_quicksort/sorting.cpp
@@ -1,44 +1,83 @@
 #include <iostream>
 #include <vector>
 #include <cstdlib>
+#include <tuple>
 
-using std::vector;
-using std::swap;
+std::tuple<int, int> part3Fun(std::vector<int> &a, int left, int right){
+  int pivot = a[right];
+  int i = left - 1;
+  int p = left - 1;
+  int j = right;
+  int q = right;
+  int swap, k;
 
-int partition2(vector<int> &a, int l, int r) {
-  int x = a[l];
-  int j = l;
-  for (int i = l + 1; i <= r; i++) {
-    if (a[i] <= x) {
-      j++;
-      swap(a[i], a[j]);
+  while (true){
+    while (a[i+1] < pivot){
+      i++;
+    }
+    i++;
+    while (a[j-1] > pivot){
+      j--;
+    }
+    j--;
+    if (i >= j){break;}
+    swap = a[i];
+    a[i] = a[j];
+    a[j] = swap;
+    if (a[i]==pivot){
+      p++;
+      swap = a[i];
+      a[i] = a[p];
+      a[p] = swap;
+    }
+    if (a[j]==pivot){
+      q--;
+      swap = a[j];
+      a[j] = a[q];
+      a[q] = swap;
     }
   }
-  swap(a[l], a[j]);
-  return j;
+  swap = a[i];
+  a[i] = a[right];
+  a[right] = swap;
+
+  j = i - 1;
+  k = left;
+  while (k<=p){
+    swap = a[k];
+    a[k] = a[j];
+    a[j] = swap;
+    j--;
+    k++;
+  }
+  i++;
+  k = right - 1;
+  while (k>=q){
+    swap = a[k];
+    a[k] = a[i];
+    a[i] = swap;
+    i++;
+    k--;
+  }
+  return std::make_tuple(j, i);
 }
 
-void randomized_quick_sort(vector<int> &a, int l, int r) {
-  if (l >= r) {
-    return;
-  }
-
-  int k = l + rand() % (r - l + 1);
-  swap(a[l], a[k]);
-  int m = partition2(a, l, r);
-
-  randomized_quick_sort(a, l, m - 1);
-  randomized_quick_sort(a, m + 1, r);
+void quicksort3(std::vector<int> &a, int left, int right){
+  if (right<=left){return;}
+  int j, i;
+  std::tie(j, i) = part3Fun(a, left, right);
+  quicksort3(a, left, j);
+  quicksort3(a, i, right);
 }
 
 int main() {
   int n;
   std::cin >> n;
-  vector<int> a(n);
+  std::vector<int> a(n);
   for (size_t i = 0; i < a.size(); ++i) {
     std::cin >> a[i];
   }
-  randomized_quick_sort(a, 0, a.size() - 1);
+  quicksort3(a, 0, a.size() - 1);
   for (size_t i = 0; i < a.size(); ++i) {
     std::cout << a[i] << ' ';
   }
--- Data_Structures_And_Algorithms-UC_SanDiego/1-Algorithmic_Toolbox/Week4/Programming_Assignments/3_improving_quicksort/sorting.py
+++ Data_Structures_And_Algorithms-UC_SanDiego/1-Algorithmic_Toolbox/Week4/Programming_Assignments/3_improving_quicksort/sorting.py
@@ -2,35 +2,59 @@
 import sys
 import random
 
-def partition3(a, l, r):
-    #write your code here
-    pass
-
-def partition2(a, l, r):
-    x = a[l]
-    j = l
-    for i in range(l + 1, r + 1):
-        if a[i] <= x:
-            j += 1
-            a[i], a[j] = a[j], a[i]
-    a[l], a[j] = a[j], a[l]
-    return j
-
-
-def randomized_quick_sort(a, l, r):
-    if l >= r:
+def QuickSort3(A, l, r):
+    if r <= l:
         return
-    k = random.randint(l, r)
-    a[l], a[k] = a[k], a[l]
-    #use partition3
-    m = partition2(a, l, r)
-    randomized_quick_sort(a, l, m - 1);
-    randomized_quick_sort(a, m + 1, r);
+    j, i = Partition3(A, l, r)
+    QuickSort3(A, l, j)
+    QuickSort3(A, i, r)
 
+
+def Partition3(A, l, r):
+    x = A[r]
+    i = l - 1
+    p = l - 1
+    j = r
+    q = r
+
+    while True:
+        while A[i + 1] < x:
+            i = i + 1
+        i = i + 1
+        while A[j - 1] > x:
+            j = j - 1
+        j = j - 1
+        if i >= j:
+            break
+        A[i], A[j] = A[j], A[i]
+        if A[i] == x:
+            p = p + 1
+            A[i], A[p] = A[p] , A[i]
+        if A[j] == x:
+            q = q - 1
+            A[j], A[q] = A[q], A[j]
+
+    A[i], A[r] = A[r], A[i]
+
+    j = i - 1
+    k = l
+    while k <= p:
+        A[k], A[j] = A[j], A[k]
+        j = j - 1
+        k = k + 1
+
+    i = i + 1
+    k = r - 1
+    while k >= q:
+        A[k], A[i] = A[i], A[k]
+        i = i + 1
+        k = k - 1
+
+    return j, i
 
 if __name__ == '__main__':
     input = sys.stdin.read()
     n, *a = list(map(int, input.split()))
-    randomized_quick_sort(a, 0, n - 1)
+    QuickSort3(a, 0, n - 1)
     for x in a:
         print(x, end=' ')
