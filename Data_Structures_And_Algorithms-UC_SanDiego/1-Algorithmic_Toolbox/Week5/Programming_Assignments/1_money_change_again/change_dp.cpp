8d5c9f344242e1f99265e82af4b403bb4865bf21
Evangelos Charalampidis <evan.charalampidis@gmail.com>
2020-12-23 19:05:23 +0000

    Week 5 Assignment 1

--- Data_Structures_And_Algorithms-UC_SanDiego/1-Algorithmic_Toolbox/Week5/Programming_Assignments/1_money_change_again/change_dp.cpp
+++ Data_Structures_And_Algorithms-UC_SanDiego/1-Algorithmic_Toolbox/Week5/Programming_Assignments/1_money_change_again/change_dp.cpp
@@ -1,8 +1,27 @@
 #include <iostream>
+#include <limits>
+#include <vector>
 
 int get_change(int m) {
-  //write your code here
-  return m / 4;
+  int element = std::numeric_limits<int>::max();
+  std::vector<int> coins {1, 3, 4};
+  std::vector<int> minCoins {};
+  int sumCoins = 0;
+  for (int i=1; i!=m+2; i++){
+    minCoins.push_back(element);
+  }
+  minCoins[0] = 0;
+  for (int i=1; i!=m+1; i++){
+    for (auto el:coins){
+      if (i>=el){
+        sumCoins = minCoins[i-el] + 1;
+        if (sumCoins<minCoins[i]){
+          minCoins[i] = sumCoins;
+        }
+      }
+    }
+  }
+  return minCoins[m];
 }
 
 int main() {
--- Data_Structures_And_Algorithms-UC_SanDiego/1-Algorithmic_Toolbox/Week5/Programming_Assignments/1_money_change_again/change_dp.py
+++ Data_Structures_And_Algorithms-UC_SanDiego/1-Algorithmic_Toolbox/Week5/Programming_Assignments/1_money_change_again/change_dp.py
@@ -2,8 +2,20 @@
 import sys
 
 def get_change(m):
-    #write your code here
-    return m // 4
+    element = float("inf")
+    minCoins = list()
+    sumCoins = 0
+    coins = [1,3,4]
+    minCoins[0] = 0
+    for i in range(1, m+2):
+        minCoins.append(element)
+    for i in range(1, m + 1):
+        for el in coins:
+            if i >= el:
+                sumCoins = minCoins[i - el] + 1
+                if sumCoins < minCoins[i]:
+                    minCoins[i] = sumCoins
+    return minCoins[money]
 
 if __name__ == '__main__':
     m = int(sys.stdin.read())
